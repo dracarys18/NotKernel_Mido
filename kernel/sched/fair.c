3be18bb4bfca4a8f99c90872f7e06fcc0b37b07a
Dušan Uverić <dusan.uveric9@gmail.com>
2018-05-30 23:08:34 +0200

    disable GENTLE_FAIR_SLEEPERS

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4770,7 +4770,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * for a gentler effect of sleepers:
 		 */
 		if (sched_feat(GENTLE_FAIR_SLEEPERS))
-			thresh >>= 1;
+			thresh >>= 0;
 
 		vruntime -= thresh;
 	}
