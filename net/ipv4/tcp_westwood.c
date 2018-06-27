46ffd4ee048fba96ddce6cdf58c672fdce4b82ad
Yaroslav Furman <yaro330@gmail.com>
2018-06-27 15:03:46 +0530

    tcp_westwood: make 'rtt_min' and 'init_rtt' tunables and remove HZ dependency
    It does seem like functions that use those values suffer __A LOT__
    after moving to 300 HZ, that causes terrible connectivity issues
    on Mata and maybe other devices with bad antennas.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -1,3 +1,4 @@
+
 /*
  * TCP Westwood+: end-to-end bandwidth estimation for TCP
  *
@@ -73,7 +74,7 @@ static void tcp_westwood_init(struct sock *sk)
 	w->accounted = 0;
 	w->cumul_ack = 0;
 	w->reset_rtt_min = 1;
-	w->rtt_min = w->rtt = TCP_WESTWOOD_INIT_RTT;
+	w->rtt_min = w->rtt = msecs_to_jiffies(tcp_westwood_init_rtt);
 	w->rtt_win_sx = tcp_jiffies32;
 	w->snd_una = tcp_sk(sk)->snd_una;
 	w->first_ack = 1;
@@ -141,7 +142,7 @@ static void westwood_update_window(struct sock *sk)
 	 * Obviously on a LAN we reasonably will always have
 	 * right_bound = left_bound + WESTWOOD_RTT_MIN
 	 */
-	if (w->rtt && delta > max_t(u32, w->rtt, TCP_WESTWOOD_RTT_MIN)) {
+	if (w->rtt && delta > max_t(u32, w->rtt, msecs_to_jiffies(tcp_westwood_rtt_min))) {
 		westwood_filter(w, delta);
 
 		w->bk = 0;
