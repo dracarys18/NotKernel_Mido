4c14bba5dd05b93470b0f79b94ac76007128b966
Danny Lin <danny@kdrag0n.dev>
2018-12-07 22:39:59 -0800

    cpu_input_boost: Expose a wake boost API
    Rather than hardcoding CONFIG_WAKE_BOOST_DURATION_MS in every driver that
    wants to kick wake boost, add a standardized function for all drivers to
    use for this purpose.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ include/linux/cpu_input_boost.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2018 Sultan Alsawaf <sultan@kerneltoast.com>.
+ */
+#ifndef _CPU_INPUT_BOOST_H_
+#define _CPU_INPUT_BOOST_H_
+
+#ifdef CONFIG_CPU_INPUT_BOOST
+void cpu_input_boost_kick(void);
+void cpu_input_boost_kick_max(unsigned int duration_ms);
+void cpu_input_boost_kick_wake(void);
+#else
+static inline void cpu_input_boost_kick(void)
+{
+}
+static inline void cpu_input_boost_kick_max(unsigned int duration_ms)
+{
+}
+static inline void cpu_input_boost_kick_wake(void)
+{
+}
+
+#endif
+
+#endif /* _CPU_INPUT_BOOST_H_ */
