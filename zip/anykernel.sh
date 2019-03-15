386213964e6d3d985b37c9c002df7b29f9b8496d
mejonsow <karuhegde4343@gmail.com>
2019-03-15 13:48:52 +0000

    Added Spectrum support

---
+++ zip/anykernel.sh
@@ -0,0 +1,49 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=NotKernel by jonsnow @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=mido
+device.name2=Redmi Note 4
+device.name3=Redmi Note 4x
+device.name4=HMNote4x
+device.name5=Redmi Note 4X
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# insert init.spectrum.rc in init.rc
+insert_line init.rc "import /init.spectrum.rc" after "import /init.trace.rc" "import /init.spectrum.rc";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
