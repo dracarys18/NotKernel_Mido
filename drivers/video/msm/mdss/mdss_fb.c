8d5b04ad7253ab01f72eddaff397e0b67d2d1fbd
tanish2k09 <tanish2k09.dev@gmail.com>
2019-05-30 18:22:06 -0700

    drivers: video: K-Lapse v5.0 (MDSS)
    - Introducing k-lapse "flow", a brightness-based scaling mode QOL enhancement that scales
    RGB points by 1 smoothly instead of instantly taking effect.
    - Major code refactor to better match the Linux Kernel coding style
    (huge thanks to kdrag0n for initial overall patch and general guidance).
    - Switched from SYS ATTR to module_param_cb and kernel param structs.
    - Shortened the code from 1k lines to 0.6k without compromising any logic or feature.
    - Changed tunable naming for sysfs to convey the idea of the function in a better way.
    - Added minute-level support for automation. Yes, you can now start or stop at, say 16:49
    - Dimmer factor (previously known as brightness_factor) is now allowed to be set as a
    percentage value, ranging from 10 to 100.
    - Updated README documentation to match v5.0 release.
    - Integrated MDSS code with a switch in klapse.h (see kcal-mdss-demo branch for full
    implementation in older version).
    - Changed some default config values.
    - Revised dimmer logic to fix "dimmer value multiplicative effect" for some users.
    The issue was changing brightness_factor values multiple times stacked them on top of
    each other, eventually causing the RGB to hit their minimum limit (usually 20 in KCAL),
    and not being able to restore full RGB without a pulse.
    
    * KLAPSE : Make default flow_freq value configurable at compile time from klapse.h
    * KLAPSE : Fix prototype warn
    * KLAPSE : Use appropriate callback for data types
    * My own twist on Kconfig

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -66,6 +66,10 @@
 #include "klapse.h"
 #endif
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -25,7 +25,7 @@
 #include "mdss_mdp.h"
 
 #ifdef CONFIG_KLAPSE
-#include "klapse.h"
+#include <linux/klapse.h>
 #endif
 
 #define DEF_PCC 0x100
@@ -136,6 +136,10 @@ static uint32_t igc_Table_RGB[IGC_LUT_ENTRIES] = {
 	240, 224, 208, 192, 176, 160, 144, 128, 112, 96, 80, 64,
 	48, 32, 16, 0
 };
+
+#ifdef CONFIG_KLAPSE
+struct kcal_lut_data *lut_cpy;
+#endif
 
 struct mdss_mdp_ctl *fb0_ctl = 0;
 
@@ -562,6 +566,33 @@ static DEVICE_ATTR(kcal_val, S_IWUSR | S_IRUGO, kcal_val_show, kcal_val_store);
 static DEVICE_ATTR(kcal_cont, S_IWUSR | S_IRUGO, kcal_cont_show,
 	kcal_cont_store);
 
+#ifdef CONFIG_KLAPSE
+void klapse_kcal_push(int r, int g, int b)
+{
+	lut_cpy->red = r;
+	lut_cpy->green = g;
+	lut_cpy->blue = b;
+
+	mdss_mdp_kcal_update_pcc(lut_cpy);
+}
+
+/* kcal_get_color() :
+ * @param : 0 = red; 1 = green; 2 = blue;
+ * @return : Value of color corresponding to @param, or 0 if not found
+ */
+unsigned short kcal_get_color(unsigned short int code)
+{
+	if (code == 0)
+		return lut_cpy->red;
+	else if (code == 1)
+		return lut_cpy->green;
+	else if (code == 2)
+		return lut_cpy->blue;
+
+	return 0;
+}
+#endif
+
 static int kcal_ctrl_probe(struct platform_device *pdev)
 {
 	int ret;
@@ -591,6 +622,10 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	mdss_mdp_kcal_update_pa(lut_data);
 	mdss_mdp_kcal_update_igc(lut_data);
 	mdss_mdp_kcal_display_commit();
+
+#ifdef CONFIG_KLAPSE
+	lut_cpy = lut_data;
+#endif
 
 	ret = device_create_file(&pdev->dev, &dev_attr_kcal);
 	ret |= device_create_file(&pdev->dev, &dev_attr_kcal_min);
