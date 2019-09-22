2713cea81a117ed44fe152adc489e9f0a6fd2067
Tanish Manku <tanish2k09.dev@gmail.com>
2019-09-22 10:22:20 -0400

    kcal & mdss: Add hooks for K-Lapse to work
    These lines of code are enabled only when K-Lapse is enabled in defconfig
    
    Co-authored-by: Tanish Manku <tanish2k09.dev@gmail.com>
    Co-authored-by: kingbri <bdashore3@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -63,6 +63,10 @@
 
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -338,6 +342,9 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 							!mfd->bl_level)) {
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, bl_lvl);
+#ifdef CONFIG_KLAPSE
+                set_rgb_slider(bl_lvl);
+#endif
 		mutex_unlock(&mfd->bl_lock);
 	}
 }
